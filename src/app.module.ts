560dd9b86aca33f37cfd79c34416bfe61268bd97
SPmfl <matheol.franco.loaiza@gmail.com>
2025-03-03 23:15:45 -0500

    first entity users crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
+import dbconfiguration from './config/dbconfiguration';
+import { DataSource } from 'typeorm';
+import { TypeOrmModule } from './datasource/typeorm.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    load: [configuration],
+    ...dbconfiguration()
+  }), TypeOrmModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
